aca9e363e62570673555c23f0e67bfc27243ecc6
Pushpagtm <pushpagautam529@gmail.com>
2023-05-29 12:10:27 +0545

    final work

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -20,6 +20,12 @@ function Navbar(props) {
             >
               Register
             </Link>
+              <Link
+              to="/login"
+              class="inline-block text-sm font-bold px-4 py-2 leading-none border rounded text-white border-gray-400 hover:border-transparent bg-[#3068f5cc] hover:text-white dark:focus:ring-gray-700 dark:hover:bg-gray-700 hover:bg-gray-100 mt-4 lg:mt-0 hover:scale-[1.3]"
+            >
+              Login
+            </Link>
         </nav>
 
             
